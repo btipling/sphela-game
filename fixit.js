0c7c2f3a8a2cb7e1647f5ad62dd8226be1a16bb4
Bjorn Tipling <bjorn@ambientchill.com>
2013-01-01 22:50:02 -0800

    Add a comment on fixit.js

--- fixit.js
+++ fixit.js
@@ -1,21 +1,17 @@
 #!/usr/local/bin/node
+/**
+ * @fileOverview Just a quick script to help fix some of the JSON when needed.
+ */
 
-console.log('running');
 
 fs = require('fs');
 
-console.log('start reading');
 regions_raw = fs.readFileSync('./sphela/lib/regions.js', 'utf8');
-console.log('still reading');
 region_info_raw = fs.readFileSync('./sphela/lib/region_info.js', 'utf8');
-console.log('start parsing');
 regions = JSON.parse(regions_raw);
-console.log('still parsing');
 region_info = JSON.parse(region_info_raw);
 regions.features.forEach(function(feature) {
-  console.log(feature);
   region_info[feature.id].name = feature.properties.name;
-  console.log('feature', region_info[feature.id]);
 });
 fs.writeFileSync('./sphela/lib/region_info_names.js',
     JSON.stringify(region_info), 'utf8');
